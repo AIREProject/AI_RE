dfa93cb430e14a1920650f5af0e2db282d6a0daf
Fillip <103233073+ByungilOh-Fillip@users.noreply.github.com>
2026-07-31 13:37:05 +0900

    [models] : Add Item Models
    -모델들을 추가하고 적용했습니다.
    -이 커밋은 바로 PR로 병합진행합니다.

--- UEProject/Source/AI_RE/Global/Actors/Public/AI_REItemActor.h
+++ UEProject/Source/AI_RE/Global/Actors/Public/AI_REItemActor.h
@@ -24,6 +24,7 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
+	virtual void OnConstruction(const FTransform& Transform) override;
 
 public:	
 	// IAI_REInteractableInterface override
