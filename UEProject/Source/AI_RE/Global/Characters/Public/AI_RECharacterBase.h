1f7f445851b096d61e1625c8d25e1ae4bacaa18e
Fillip <103233073+ByungilOh-Fillip@users.noreply.github.com>
2026-08-03 17:33:22 +0900

    [feat] : Add AbilitySystemInterface
    - CharacterBase에 AbilitySystemInterface를 상속
    - PossessedBy 및 어빌리티 시스템 컴포넌트 추가
    - 컴포넌트 Getter 설정

--- UEProject/Source/AI_RE/Global/Characters/Public/AI_RECharacterBase.h
+++ UEProject/Source/AI_RE/Global/Characters/Public/AI_RECharacterBase.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "AbilitySystemInterface.h"
 #include "AI_RECharacterBase.generated.h"
 
 class UAI_REStatusComponent;
@@ -13,12 +14,15 @@ class UAI_RESkillComponent;
  * Base character class that provides common functionality for all characters (Player, NPC, Monster)
  */
 UCLASS(abstract)
-class AI_RE_API AAI_RECharacterBase : public ACharacter
+class AI_RE_API AAI_RECharacterBase : public ACharacter, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
 
 public:
 	AAI_RECharacterBase();
+
+	virtual class UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	virtual void PossessedBy(AController* NewController) override;
 
 protected:
 	virtual void BeginPlay() override;
@@ -31,6 +35,10 @@ protected:
 	// 스킬 컴포넌트
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Base Components")
 	TObjectPtr<UAI_RESkillComponent> SkillComponent;
+
+	// 어빌리티 시스템 컴포넌트 (GAS)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Abilities", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<class UAbilitySystemComponent> AbilitySystemComponent;
 
 public:
 	FORCEINLINE TObjectPtr<UAI_REStatusComponent> GetStatusComponent() const { return StatusComponent; }
